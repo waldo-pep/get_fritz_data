c61045fa572ddf691b92f935fc5daad947ced495
otti <otti@192.168.1.250>
2022-12-27 10:37:49 +0100

    update script

--- get_fritz_dsl_stats.sh
+++ get_fritz_dsl_stats.sh
@@ -50,23 +50,8 @@ if [ $result -gt 0 -o $sid = "0000000000000000" ]; then
 fi
 sid=$(cat $sidfile)
 
-# --------------------------------
-# read DOCSIS-Infos (page=docInfo)
-# --------------------------------
-#docsis=$(curl -s "http://$fritzbox/data.lua" -d "xhr=1&sid=$sid&lang=de&page=docInfo&xhrId=all&no_sidrenew")
-
-#echo "docsis=$docsis"
-
-#my_docsis=$(curl -s "http://$fritzbox/internet/dsl_spectrum.lua" -d "xhr=1&sid=$sid&lang=de&page=docInfo&xhrId=all&no_sidrenew" -H "Content-Type:application/json")
-#my_docsis=$(curl --insecure -d "sid=$sid&lang=de&page=docInfo&" -X POST "http://$fritzbox/data.lua")
-
-#response=$(curl -s "http://$fritzbox/internet/dsl_stats_tab.lua" -d "xhr=1&sid=$sid&lang=de&page=docInfo&xhrId=all")
-#echo "$response" > dsl_stats_tab.html
-#npx prettier --write dsl_stats_tab.html 
-
-#response=$(curl -s "http://$fritzbox/internet/dsl_stats_graph.lua" -d "xhr=1&sid=$sid&lang=de&page=docInfo&xhrId=all")
-#echo "$response" > dsl_stats_graph.html
-#npx prettier --write dsl_stats_graph.html 
+# no more requests, disable the login 
+# (made at the last request to the fritzbox)
 last_request="&no_sidrenew"
 
 ###################################################################
@@ -80,7 +65,7 @@ today_online=$(cat inetstat_counter.html | grep -m 1 "Online-Zeit (hh:mm)" | cut
 today_sum=$(cat inetstat_counter.html | grep -m 1 "Online-Zeit (hh:mm)" | cut -d ">" -f 4 | cut -d "<" -f 1)
 today_send=$(cat inetstat_counter.html | grep -m 1 "Online-Zeit (hh:mm)" | cut -d ">" -f 6 | cut -d "<" -f 1)
 today_received=$(cat inetstat_counter.html | grep -m 1 "Online-Zeit (hh:mm)" | cut -d ">" -f 8 | cut -d "<" -f 1)
-echo "$today_online, $today_sum, $today_send, $today_received"
+#echo "$today_online, $today_sum, $today_send, $today_received"
 if [ -f "$path/fritzBox_net.csv" ]; then 
   echo "$(date '+%Y-%m-%d');$(date '+%T');$today_online,$today_sum,$today_send,$today_received" >> "$path/fritzBox_net.csv"
 else 
@@ -93,58 +78,3 @@ fi
 #echo "$response" > inetstat_monitor.html
 #npx prettier --write inetstat_monitor.html 
 
-#echo "my_docsis=$my_docsis"
-#cat dsl_stats_tab_lua.html |grep DSLAM-Datenrate
-#echo $my_docsis
-exit
-# AVM may change the modulation entry from 'type' to 'modulation' in later OS version
-if [ "_"$(echo ${docsis} | jq -r ".data.channelUs.docsis30[0].type") = "_null" ]; then
-  qam="modulation"
-else
-  qam="type"
-fi
-
-echo "3 $qam"
-
-# get nr. of up-/downstream channels
-channelUs=$(echo ${docsis} | jq ".data.channelUs.docsis30[].powerLevel" | wc -l)
-channelDs=$(echo ${docsis} | jq ".data.channelDs.docsis30[].powerLevel" | wc -l)
-
-echo "3.5"
-
-# read upstream channels
-for (( c=0; c<$channelUs; c++ )); do
-  channelID[$c]=$(echo ${docsis}  | jq -r ".data.channelUs.docsis30[$c].channelID")
-  channel[$c]=$(echo ${docsis}    | jq -r ".data.channelUs.docsis30[$c].channel")
-  modulation[$c]=$(echo ${docsis} | jq -r ".data.channelUs.docsis30[$c].$qam" | sed 's/[^0-9.]//g')
-  powerLevel[$c]=$(echo ${docsis} | jq -r ".data.channelUs.docsis30[$c].powerLevel")
-  frequency[$c]=$(echo ${docsis}  | jq -r ".data.channelUs.docsis30[$c].frequency")
-
-  echo "docsis,mode=up,channel=${channelID[$c]} Modulation=${modulation[$c]}"
-  echo "docsis,mode=up,channel=${channelID[$c]} PowerLevel=${powerLevel[$c]}"
-  echo "docsis,mode=up,channel=${channelID[$c]} Frequenz=${frequency[$c]}"
-done
-
-echo "4"
-
-
-# read downstream channels
-for (( c=0; c<$channelDs; c++ )); do
-  channelID[$c]=$(echo ${docsis}  | jq -r ".data.channelDs.docsis30[$c].channelID")
-  channel[$c]=$(echo ${docsis}    | jq -r ".data.channelDs.docsis30[$c].channel")
-  modulation[$c]=$(echo ${docsis} | jq -r ".data.channelDs.docsis30[$c].$qam" | sed 's/[^0-9.]//g')
-  powerLevel[$c]=$(echo ${docsis} | jq -r ".data.channelDs.docsis30[$c].powerLevel")
-  frequency[$c]=$(echo ${docsis}  | jq -r ".data.channelDs.docsis30[$c].frequency")
-  latency[$c]=$(echo ${docsis}    | jq -r ".data.channelDs.docsis30[$c].latency")
-  corrErrors[$c]=$(echo ${docsis} | jq -r ".data.channelDs.docsis30[$c].corrErrors")
-  nonCorrErrors[$c]=$(echo ${docsis} | jq -r ".data.channelDs.docsis30[$c].nonCorrErrors")
-
-  echo "docsis,mode=down,channel=${channelID[$c]} Modulation=${modulation[$c]}"
-  echo "docsis,mode=down,channel=${channelID[$c]} PowerLevel=${powerLevel[$c]}"
-  echo "docsis,mode=down,channel=${channelID[$c]} Frequenz=${frequency[$c]}"
-  echo "docsis,mode=down,channel=${channelID[$c]} Latenz=${latency[$c]}"
-  echo "docsis,mode=down,channel=${channelID[$c]} korrFehler=${corrErrors[$c]}"
-  echo "docsis,mode=down,channel=${channelID[$c]} Fehler=${nonCorrErrors[$c]}"
-done
-
-echo "finish"
